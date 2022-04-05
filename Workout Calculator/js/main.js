b2f6f4fb07b21e5b0c9e8e414af467541899b1c1
turbokungfu <slashrandazzo@gmail.com>
2022-04-04 18:37:12 -0700

    worked CSS and trying to fix JS counter

--- Workout Calculator/js/main.js
+++ Workout Calculator/js/main.js
@@ -1,78 +1,44 @@
+
+
+
 document.querySelector('#calorie').addEventListener('click', pizzaSize)
 
 function pizzaSize(){
     let size = document.querySelector('#pizzaSize').value
     if (size <500){
-        document.querySelector('#pizza').style.width = '14rem'
+        document.querySelector('#pizza').style.width = '4rem'
     }
     else if (size >= 500 && size< 1000){
-        document.querySelector('#pizza').style.width = '24rem'
+        document.querySelector('#pizza').style.width = '9rem'
     }
     else if (size >=1000 && size < 2000){
-        document.querySelector('#pizza').style.width = '44rem'
+        document.querySelector('#pizza').style.width = '12rem'
     }
     else{
-        document.querySelector('#pizza').style.width = '64rem'
+        document.querySelector('#pizza').style.width = '15rem'
     }
-  
-
 }
 
+    
+document.querySelector('#calorie').addEventListener('click', timer)
 
+function timer(){
+    let time = document.querySelector('#workoutTime').value*60000
+    console.log(time)
+    
+    while (time > 0){
+        setInterval(decreasePizza, [1000])
+        time = time - 1000
+}
 
-
-
-// document.querySelector('#pizzaSelect').addEventListener('click', pizzaSize)
-
-// function pizzaSize(){
-//     if (document.querySelector('#small')){
-//     document.querySelector('#pizza').style.width = '14rem'
-// }
-// else if (document.querySelector('#medium')){
-//     document.querySelector('#pizza').style.width = '34rem'
-// }
-// else if (document.querySelector('#large')){
-//     document.querySelector('#pizza').style.width = '54rem'
-// }}
-
-//don't know why this doesn't work:   document.querySelector('#pizza').style.width = ('size * 1rem')
-
-// const smallPizza = document.querySelector('#pizzaSelect').addEventListener('change', pizzaSize)
-
-//document.getElementById('pizzaSelect').onchange = pizzaSize
-
-
-
-
-// function pizzaSmall(){
-//     document.querySelector('#pizza').style.width = '14rem'
-// }
-// function pizzaMedium(){
-//     document.querySelector('#pizza').style.width = '34rem'
-// }
-// function pizzaLarge(){
-//     document.querySelector('#pizza').style.width = '54rem'
-// }
-
-
-// function pizzaSize(){
-//    if(document.getElementById('small')){
-//     document.getElementById('pizza').style.width = '4rem'
-//    }
-//    else if(document.getElementById('medium')){
-//     document.getElementById('pizza').style.width = '24rem'
-//    } 
-//    else if(document.getElementById('large')){
-//     document.getElementById('pizza').style.width = '44rem'
-//    }
-// }
-
-
-
-// const small = document.querySelector('#small')
-// const medium = document.querySelector('#medium')
-// const large = document.querySelector('#large')
-
-// if(small){document.querySelector('#pizza').style.width = '4rem'}
-// if(medium){document.querySelector('#pizza').style.width = '14rem'}
-// if(large){document.querySelector('#pizza').style.width = '44rem'}
+function decreasePizza(){
+    console.log('pizza')
+}
+}
+    // setInterval("countdown()",1000){
+    //     console.log(time)
+    //     time--
+    //     if (time === 0){
+    //         alert('Pizza gone');
+    //         }}
+    // countdownId = setInterval("countdown()", 1000)}
